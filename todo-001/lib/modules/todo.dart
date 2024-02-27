ec439d12a5d0cc4e8f35cf06a755a044035da599
yangtory <wjdduscldrn@naver.com>
2024-02-27 14:43:31 +0900

    수정

---
+++ todo-001/lib/modules/todo.dart
@@ -0,0 +1,15 @@
+class Todo {
+  // 데이터 클래스
+  String sdate;
+  String stime;
+  String content;
+  bool complete;
+
+// 생성자
+  Todo({
+    required this.sdate,
+    required this.stime,
+    required this.content,
+    required this.complete,
+  });
+}
--- todo-001/lib/modules/todo_app.dart
+++ todo-001/lib/modules/todo_app.dart
@@ -36,13 +36,6 @@ class _TodoAppState extends State<TodoApp> {
               color: Colors.white,
             ),
           ),
-          IconButton(
-            onPressed: () => {},
-            icon: const Icon(
-              Icons.add_alarm,
-              color: Colors.white,
-            ),
-          ),
         ],
       ),
     );
@@ -54,26 +47,54 @@ class _TodoAppState extends State<TodoApp> {
       builder: (context) {
         return AlertDialog(
           title: const Text("할일 등록"),
-          // TextField input box 나타남
           actions: [
-            TextField(
-              textInputAction: TextInputAction.go,
-              // hintText: placeholder
-              decoration: const InputDecoration(hintText: "할 일을 입력해 주세요"),
-              onSubmitted: (value) {
-                // SnackBar 를 띄우기 위해 snackBar 객체(변수) 선언
-                SnackBar snackBar = const SnackBar(
-                  content: Text("할일이 등록됨"),
-                );
-                // ScaffoldMessenger 에게 snackBar 를 표시해줘 라고 요청
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                // alert dialog 를 닫아라
-                Navigator.of(context).pop();
-              },
-            )
+            // TextField input box 나타남
+            _todoInputBox(context)
           ],
         );
       },
+    );
+  }
+
+  Widget _todoInputBox(BuildContext context) {
+    return Padding(
+      // 테두리를 8.0만큼
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              /// Expanded Widget
+              /// coolumn, row 등으로 widget 을 감싸면 content 가 없는경우
+              /// widget 이 화면에서 사라져 버리는 경우가 있다
+              /// 이 때는 그 widget 을 Expanded Widget으로 감싸주면 해결
+              Expanded(
+                child: TextField(
+                  textInputAction: TextInputAction.go,
+                  // hintText: placeholder
+                  decoration: const InputDecoration(hintText: "할 일을 입력해 주세요"),
+                  onSubmitted: (value) {
+                    // SnackBar 를 띄우기 위해 snackBar 객체(변수) 선언
+                    SnackBar snackBar = const SnackBar(
+                      content: Text("할일이 등록됨"),
+                    );
+                    // ScaffoldMessenger 에게 snackBar 를 표시해줘 라고 요청
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    // alert dialog 를 닫아라
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              IconButton(
+                onPressed: () => {},
+                icon: const Icon(
+                  Icons.send_outlined,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 } // todo state end
