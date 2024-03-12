fb7f6c5ae743ef2b9599bed419f43a796eba837b
yangtory <wjdduscldrn@naver.com>
2024-03-12 11:50:14 +0900

    todo

--- todo-002/lib/main.dart
+++ todo-002/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo/model/todo.dart';
+import 'package:todo/service/todo_service.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(const App());
@@ -30,64 +34,117 @@ class _TodoApHomeState extends State<TodoHome> {
   final todoInputController = TextEditingController();
   String todo = "";
 
+  getTodo(String todo) {
+    return Todo(
+      id: const Uuid().v4(),
+      // 현재 디바이스의 날짜를 가져와서 (Datetime.now())
+      // 문자열 형식으로 변환하라
+      sdate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
+      stime: DateFormat("HH:ss:mm").format(DateTime.now()),
+      edate: "",
+      etime: "",
+      content: todo,
+      complete: false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("TODO List"),
-      ),
-      body: const Center(
-        child: Text(
-          "안녕하세요",
-          style: TextStyle(fontSize: 30),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("TODO List"),
         ),
-      ),
-      // 화면이 찌그러질때 유지할수 있게
-      bottomSheet: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(
-            bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-          child: Container(
-            width: double.infinity,
-            color: Colors.white,
-            // textfield 에서 padding으로 감싸기
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: TextField(
-                controller: todoInputController,
-                onChanged: (value) => {
-                  setState(() {
-                    todo = value;
-                  })
+        bottomSheet: bottomSheet(context),
+        // FutureBuilder
+        // 데이터베이스 에서 가져온 List<Todo> 데이터를 화면에 표현하기 위한
+        // 빌더(생성자, 만들기 함수)
+        body: todoListBody(context));
+  }
+
+//todoListBody
+  Widget todoListBody(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        // width 최대값으로 늘리기
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          FutureBuilder(
+            // selectAll() 한데이터를 future 속성에 주입
+            future: TodoService().selectAll(),
+            // snpashot : 데이터의 복사본
+            // 내부에서 가공되어 builder 의 snapshot 에 데이터 전달하고
+            builder: (context, snapshot) {
+              // snpashot.data 의 데이터를 List<Todo> 타입으로 변환하여 할당
+              var todoList = snapshot.data as List<Todo>;
+              // ListView.builder
+              // todoList snapshot data를 가지고 실제 List 를 화면에 그리기
+              return ListView.builder(
+                shrinkWrap: true,
+                // 이 개수 만큼 화면을 그려
+                itemCount: todoList.length,
+                itemBuilder: (context, index) {
+                  return Text(todoList[index].content);
                 },
-                decoration: InputDecoration(
-                  hintText: "할일을 입력하세요",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  //SizeBox : widget 사이에 간격을 줄 때 박스 넣기
-                  // textfiled 안에 줬기 때문에 커서 위치 조절할 수 잇슴
-                  prefix: const SizedBox(width: 20),
-                  suffix: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: () => {todoInputController.clear()},
-                        icon: const Icon(Icons.clear),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          var snackBar = SnackBar(content: Text(todo));
-                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                          // send 한 다음 input box에 있는거 지워
-                          todoInputController.clear();
-                        },
-                        icon: const Icon(Icons.send_outlined),
-                      ),
-                    ],
-                  ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+//bottomSheet
+  Widget bottomSheet(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: Container(
+          width: double.infinity,
+          color: Colors.white,
+          // textfield 에서 padding으로 감싸기
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              controller: todoInputController,
+              onChanged: (value) => {
+                setState(() {
+                  todo = value;
+                })
+              },
+              decoration: InputDecoration(
+                hintText: "할일을 입력하세요",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                //SizeBox : widget 사이에 간격을 줄 때 박스 넣기
+                // textfiled 안에 줬기 때문에 커서 위치 조절할 수 잇슴
+                prefix: const SizedBox(width: 20),
+                suffix: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () => {todoInputController.clear()},
+                      icon: const Icon(Icons.clear),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        var snackBar = SnackBar(content: Text(todo));
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                        var todoDto = getTodo(todo);
+
+                        // send 후 키보드 감추기
+                        FocusScope.of(context).unfocus();
+                        await TodoService().insert(todoDto);
+                        // send 한 다음 input box에 있는거 지워
+                        todoInputController.clear();
+                      },
+                      icon: const Icon(Icons.send_outlined),
+                    ),
+                  ],
                 ),
               ),
             ),
--- todo-002/lib/service/todo_service.dart
+++ todo-002/lib/service/todo_service.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: constant_identifier_names, unused_field
+// ignore_for_file: constant_identifier_names, unused_field, unused_local_variable
 
+import 'package:flutter/foundation.dart';
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:todo/model/todo.dart';
 
 const TBL_TODO = "tbl_todolist";
 // """ : 연속된 문자열
@@ -11,23 +14,84 @@ CREATE TABLE $TBL_TODO (
   stime TEXT,
   edate TEXT,
   etime TEXT,
+  content TEXT,
   complete INTEGER
 )
 """;
 
 class TodoService {
   late Database _database;
+  // Future<Database> get database() async {
+  //   _database = await
+  // }
 
   onCreateTable(db, version) async {
     return db.execute(createTodoTable);
   }
 
 // 스키마 변경 시 기존 테이블 제거 후 새로 테이블 만들기
+// initData / openDatabase 가 실행될 때
+// version 번호를 비교하여 새로운 version 번호가 있으면
+// Table 의 구조를 변경한다.
   onUpgradeTable(db, oldVersion, newVersion) async {
-    // batch : 트랜젝션 발생할 때 이렇게 해줘
-    final batch = await db.batch();
-    await batch.execute("DROP TABLE $TBL_TODO");
-    await batch.execute(createTodoTable);
-    await batch.commit();
+    if (newVersion > oldVersion) {
+      debugPrint("$oldVersion, $newVersion");
+      // batch : 트랜젝션 발생할 때 이렇게 해줘
+      final batch = await db.batch();
+      await batch.execute("DROP TABLE $TBL_TODO");
+      await batch.execute(createTodoTable);
+      await batch.commit();
+    }
+  }
+
+  Future<Database> initDatabase() async {
+    // getDatabasePath : 스마트 기기의 DB 저장소 폴더 위치를 가져오는 함수
+    String dbPath = await getDatabasesPath();
+
+    // 저장소에 저 이름의 폴더를 만들어 쓰겠다
+    // join 폴더1/폴더2/폴더3/todo.dbf 라는 경로를 설정할 때
+    // 운영체제마다 dir seperator 문자가 다르다.
+    // "폴더1" + "/" + "폴더2" + "/" + "폴더3" 어떤 운영체제는 /,
+    // "폴더1" + "\" + "폴더2" + "\" + "폴더3" 어떤 운영체제는 \ 사용하는데
+    // path.join() 은 운영체제에 맞춰서 구분자(dir seperator)를 만들어준다.
+    String dbFile = join(dbPath, "todo.dbf");
+
+    // 테이블 없으며면 create, upgrade 해야하면 그거해
+    return await openDatabase(
+      dbFile,
+      onCreate: onCreateTable,
+      onUpgrade: onUpgradeTable,
+      version: 2,
+    );
+  }
+
+  // DB 를 사용할 수 있도록 open 하고 연결정보가 담긴 _database 변수를 초기화 한다
+  // flutter 에서 사용하는 특이한 getter method
+  // 이 함수는 () 가 없고, 함수 이름 앞에 get 키워드가 있다
+  // 이 함수 이름과 연관된 로컬변수(_함수) 와 같은 형식의 변수가 있어야 한다.
+  Future<Database> get database async {
+    _database = await initDatabase();
+    return _database;
+  }
+
+// todo를 매개변수로 받아서 tbl_todo에다 todo데이터 insert
+// getter method와 연결돼 있어서 ()가 없다
+  insert(Todo todo) async {
+    final db = await database;
+    debugPrint("INSERT TO : ${todo.toString()}"); // 확인하기
+    var result = await db.insert(TBL_TODO, todo.toMap());
+  }
+
+  // Future : 동적 데이터에 대한 약속(promise)
+  // selectAll() 함수를 호출하면 반드시 List<Todo> 데이터 타입을 return 하겠다.
+  Future<List<Todo>> selectAll() async {
+    final db = await database; //db연결
+    //final todoList = await db.query((TBL_TODO));
+    ////데이터가져오기, db.query가 어떤 데이터를  return하는지 모를경우 final만 쓸수있다
+    final List<Map<String, dynamic>> todoList =
+        await db.query(TBL_TODO); // 위 코드와 같다
+    final result = List.generate(todoList.length,
+        (index) => Todo.fromMap(todoList[index])); //foreach와 비슷
+    return result;
   }
 }
